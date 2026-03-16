5c4a00d01e4b163e99cfebd7244b7b4d566d2fb5
ARandomGuy321 <tomik201212@gmail.com>
2026-03-16 16:52:46 +0500

    Update CCKeyboardDispatcher.cpp

--- src/hooks/cocos2d/CCKeyboardDispatcher.cpp
+++ src/hooks/cocos2d/CCKeyboardDispatcher.cpp
@@ -17,3 +17,26 @@ void PSCCKeyboardDispatcher::removeDelegate(cocos2d::CCKeyboardDelegate* i_deleg
     }
     CCKeyboardDispatcher::removeDelegate(i_delegate);
 }
+
+bool PSCCKeyboardDispatcher::dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool down, bool isArrowKey) {
+    if (down) {
+        auto* pl = PlayLayer::get();
+        if (pl) {
+            auto* ppl = static_cast<PSPlayLayer*>(pl);
+            auto keybind = Mod::get()->getSettingValue<keybinds::Keybind>("save-keybind");
+            if (keybind.key == key && ppl->canSave() && ppl->startSaveGame()) {
+                PSPauseLayer* l_pauseLayer = static_cast<PSPauseLayer*>(CCScene::get()->getChildByID("PauseLayer"));
+                if (l_pauseLayer) {
+                    if (l_pauseLayer->m_fields->m_saveCheckpointsSprite != nullptr)
+                        l_pauseLayer->m_fields->m_saveCheckpointsSprite->setColor({127,127,127});
+                    if (l_pauseLayer->m_fields->m_saveCheckpointsSprite != nullptr && l_pauseLayer->m_fields->m_saveCheckpointsSprite->getChildren()->count() > 0)
+                        static_cast<CCSprite*>(l_pauseLayer->m_fields->m_saveCheckpointsSprite->getChildren()->objectAtIndex(0))->setColor({127,127,127});
+                    if (l_pauseLayer->m_fields->m_saveCheckpointsButton != nullptr)
+                        l_pauseLayer->m_fields->m_saveCheckpointsButton->m_bEnabled = false;
+                }
+                return true;
+            }
+        }
+    }
+    return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, isArrowKey);
+}
