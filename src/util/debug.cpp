81e46cd6e9bb92e1d070bfe446331006eec43899
ARandomGuy321 <tomik201212@gmail.com>
2026-03-20 17:58:08 +0500

    Update debug.cpp

--- src/util/debug.cpp
+++ src/util/debug.cpp
@@ -11,3 +11,18 @@ void readSeparator(std::string o_string, Stream& i_stream) {
 }
 #endif
 #endif
+
+
+
+$execute {
+    auto base = geode::base::get();
+    auto addr = base + 0x3a32d0; // tryPlaceCheckpoint function
+
+    geode::hook::create((void*)addr, +[](long long self) {
+        auto original = (void(*)(long long)) geode::hook::getOriginal();
+
+        log::info("[HOOK] tryPlaceCheckpoint BEFORE original");
+        original(self);
+        log::info("[HOOK] tryPlaceCheckpoint AFTER original");
+    });
+}
